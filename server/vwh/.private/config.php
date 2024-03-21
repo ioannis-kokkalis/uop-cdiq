eee24fbf6ee5620afb3951d309635c5a2ea83809
Ioannis Kokkalis <ioanniskokkalis2001@gmail.com>
2024-03-21 11:22:10 +0200

    initial

---
+++ server/vwh/.private/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$config = [
+	"dbms_host" => getenv('DB_HOST'),
+	"dbms_db_name" => getenv('DB_NAME'),
+	"dbms_username" => getenv('DB_USERNAME'),
+	"dbms_password" => getenv('DB_PASSWORD'),
+];
