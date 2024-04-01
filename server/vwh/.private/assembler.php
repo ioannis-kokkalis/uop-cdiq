344113f9ea0b33d575d9b1b71a1b338de51736a6
Ioannis Kokkalis <ioanniskokkalis2001@gmail.com>
2024-04-01 18:06:36 +0300

    rework page building, footer responsiveness, minor css updates

---
+++ server/vwh/.private/assembler.php
@@ -0,0 +1,138 @@
+<?php
+
+enum Stylesheet : string {
+	case Main = "/style/main.css";
+}
+
+class Assembler {
+
+	private string		$body_header_title;
+
+	public string		$head_title;
+	public Stylesheet 	$head_stylesheet;
+	public Closure		$body_main;
+
+	public function __construct(string $body_header_title) {
+		$this->head_title = 'UoP CDIQ 2024';
+		$this->head_stylesheet = Stylesheet::Main;
+
+		$this->body_header_title = $body_header_title;
+		$this->body_main = function() { ?><p>This page has no content yet.</p><?php };
+	}
+
+	public function assemble() : void {
+		header('cache-control: no-cache, no-store, must-revalidate');
+		?>
+		<!DOCTYPE html>
+		<html lang="en">
+		<head><?php $this->head(); ?></head>
+		<body><?php $this->body(); ?></body>
+		</html>
+		<?php
+	}
+
+	protected function head() : void {
+		?>
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<link rel="stylesheet" href="<?= $this->head_stylesheet->value ?>">
+			<title><?= $this->head_title ?></title>
+		</head>
+		<?php
+	}
+
+	protected function body() : void {
+		?>
+
+		<header>
+			<nav><?=  $this->body_header_nav() ?></nav>
+
+			<hr>
+
+			<h1 align="center"><?= $this->body_header_title ?></h1>
+		</header>
+		
+		<hr>
+
+		<main>
+			<?= call_user_func($this->body_main); ?>
+		</main>
+
+		<div class="spacer"></div>
+
+		<hr>
+
+		<footer>
+			<p align="center">
+				<a href="https://www.uop.gr/">University of the Peloponnese</a> © Career Day Interviews Queuing 2024
+			</p>
+		</footer>
+
+		<?php
+	}
+
+	protected function body_header_nav() : void {
+		?>
+		<a href="/">Home</a>
+		<a href="/queues.php">Queues</a>
+		<a href="/suggestions.php">Suggestions</a>
+		<?php
+	} 
+
+}
+
+enum Operator : string {
+	case Secretary = 'secretary';
+	case Gatekeeper = 'gatekeeper';
+}
+
+class AssemblerOperate extends Assembler {
+
+	private static string $SESSION_OPERATOR_INDEX = "session_operator_index";
+
+	public function __construct(string $body_header_title) {
+		parent::__construct($body_header_title);
+
+		$this->head_title = 'Operate: ' . $this->head_title;
+
+		session_start();
+	}
+
+	protected function body_header_nav() : void {
+		?>
+		<a href="/">Home</a>
+		<a href="/costas/vasilakis.php">Operate</a>
+		<?php
+	}
+
+	public function operator_challenge(string $password) : false {
+		require_once $_SERVER['DOCUMENT_ROOT'] . '/.private/database.php';
+		$db = database();
+
+		$type = $db->operator_mapping($password) ?? '';
+		$type = Operator::tryFrom($type);
+
+		if( $type === null ) {
+			return false;
+		}
+
+		$_SESSION[AssemblerOperate::$SESSION_OPERATOR_INDEX] = $type;
+		header("Location: /costas/{$type->value}.php");
+		exit;
+	}
+
+	public function operator_ensure(Operator $operator) {
+		if( isset($_SESSION[AssemblerOperate::$SESSION_OPERATOR_INDEX]) === false
+			|| $_SESSION[AssemblerOperate::$SESSION_OPERATOR_INDEX] !== $operator
+		) {
+			header('Location: /costas/vasilakis.php');
+			exit;
+		}
+	}
+
+	function operator_clear() {
+		unset($_SESSION[AssemblerOperate::$SESSION_OPERATOR_INDEX]);
+	}
+
+}
--- server/vwh/suggestions.php
+++ server/vwh/suggestions.php
@@ -1,162 +1,148 @@
 <?php
-	define('MODE_GUEST', 'GUEST');
-	define('MODE_LOGIN', 'LOGIN');
 
-	$showInitial = false;
-	$showFileUploadForm = false;
-	$showSuggestionsResult = false;
+define('MODE_GUEST', 'GUEST');
+define('MODE_LOGIN', 'LOGIN');
 
-	if(isset($_POST['submit'])) {
-		$showFileUploadForm = true;
+$showInitial = false;
 
-		$mode = isset($_POST['mode']) ? $_POST['mode'] : NULL;
-		if($mode == MODE_GUEST) {
-			// skip
-		}
-		else if($mode == MODE_LOGIN) {
-			// $return_url = 'http://' . "placeholderhost:port" . $_SERVER['REQUEST_URI'];
-			// $sso_url = 'https://sso.uop.gr/login?service=' . urlencode($return_url);
-			// header('Location: ' . $sso_url);
-			// exit();
-			// /* TODO look it up with Costas?
-			// Application Not Authorized to Use CAS. The application you attempted to authenticate to is not authorized to use CAS. This usually indicates that the application is not registered with CAS, or its authorization policy defined in its registration record prevents it from leveraging CAS functionality, or it's malformed and unrecognized by CAS. Contact your CAS administrator to learn how you might register and integrate your application with CAS.
-			// */
-		}
-		else {
-			header('Location: /suggestions.php');
-			exit();
-		}
+$showFileUploadForm = false;
+$mode = NULL;
+
+$showSuggestionsResult = false;
+$suggestionResult = NULL;
+
+if(isset($_POST['submit'])) {
+	$showFileUploadForm = true;
+
+	$mode = isset($_POST['mode']) ? $_POST['mode'] : NULL;
+	if($mode == MODE_GUEST) {
+		// skip
 	}
-	// else if (returning from SSO) {
-	// 	$showFileUploadForm = true;
-	// 	// TODO
-	// 	$mode = MODE_LOGIN;
-	// 	$email = sso.email;
-	// }
-	else if(isset($_FILES['resume'])) {
-		$showSuggestionsResult = true;
-		
-		$suggestionResult = "This feature is not yet implemented.";
-
-		if($_FILES['resume']['error'] == UPLOAD_ERR_NO_FILE) {
-			$suggestionResult = "No file uploaded!";
-		}
-		//else if($_FILES['resume']['error'] == UPLOAD_ERR_OK) { // TODO consider size maybe?
-		// 	$fileData = curl_file_create($_FILES['resume']['tmp_name'], $_FILES['resume']['type'], $_FILES['resume']['name']);
-
-		// 	$curl = curl_init();
-	
-		// 	curl_setopt_array($curl, [
-		// 		CURLOPT_PORT => "12345",
-		// 		CURLOPT_URL => "http://192.168.48.222/classify_resume", // TODO use the container name when added in compose.yml
-		// 		CURLOPT_RETURNTRANSFER => true,
-		// 		CURLOPT_ENCODING => "",
-		// 		CURLOPT_MAXREDIRS => 10,
-		// 		CURLOPT_TIMEOUT => 0,
-		// 		CURLOPT_FOLLOWLOCATION => true,
-		// 		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		// 		CURLOPT_CUSTOMREQUEST => "POST",
-		// 		CURLOPT_POSTFIELDS => ["file" => $fileData],
-		// 		CURLOPT_HTTPHEADER => [
-		// 			"accept: application/json",
-		// 			"Content-Type: multipart/form-data",
-		// 		],
-		// 	]);
-			
-		// 	$response = curl_exec($curl);
-		// 	curl_close($curl);
-		
-		// 	$suggestionResult = $response; // TODO return some structure with the result or error not being able to open the file
-		// }
+	else if($mode == MODE_LOGIN) {
+		// $return_url = 'http://' . "placeholderhost:port" . $_SERVER['REQUEST_URI'];
+		// $sso_url = 'https://sso.uop.gr/login?service=' . urlencode($return_url);
+		// header('Location: ' . $sso_url);
+		// exit();
+		// /* TODO look it up with Costas?
+		// Application Not Authorized to Use CAS. The application you attempted to authenticate to is not authorized to use CAS. This usually indicates that the application is not registered with CAS, or its authorization policy defined in its registration record prevents it from leveraging CAS functionality, or it's malformed and unrecognized by CAS. Contact your CAS administrator to learn how you might register and integrate your application with CAS.
+		// */
 	}
 	else {
-		$showInitial = true;
+		header('Location: /suggestions.php');
+		exit();
 	}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="/style/main.css">
-	<title>UoP CDIQ 2024</title>
-</head>
-<body>
-	<?php include $_SERVER['DOCUMENT_ROOT'] . '/.private/components/navigation.php'; ?>
-
-	<hr>
-
-	<header>
-		<h1><center>Suggestions for Interviews</center></h1>
-	</header>
+}
+// else if (returning from SSO) {
+// 	$showFileUploadForm = true;
+// 	// TODO
+// 	$mode = MODE_LOGIN;
+// 	$email = sso.email;
+// }
+else if(isset($_FILES['resume'])) {
+	$showSuggestionsResult = true;
 	
-	<hr>
+	$suggestionResult = "This feature is not yet implemented.";
 
-	<?php
+	if($_FILES['resume']['error'] == UPLOAD_ERR_NO_FILE) {
+		$suggestionResult = "No file uploaded!";
+	}
+	//else if($_FILES['resume']['error'] == UPLOAD_ERR_OK) { // TODO consider size maybe?
+	// 	$fileData = curl_file_create($_FILES['resume']['tmp_name'], $_FILES['resume']['type'], $_FILES['resume']['name']);
+
+	// 	$curl = curl_init();
+	
+	// 	curl_setopt_array($curl, [
+	// 		CURLOPT_PORT => "12345",
+	// 		CURLOPT_URL => "http://192.168.48.222/classify_resume", // TODO use the container name when added in compose.yml
+	// 		CURLOPT_RETURNTRANSFER => true,
+	// 		CURLOPT_ENCODING => "",
+	// 		CURLOPT_MAXREDIRS => 10,
+	// 		CURLOPT_TIMEOUT => 0,
+	// 		CURLOPT_FOLLOWLOCATION => true,
+	// 		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	// 		CURLOPT_CUSTOMREQUEST => "POST",
+	// 		CURLOPT_POSTFIELDS => ["file" => $fileData],
+	// 		CURLOPT_HTTPHEADER => [
+	// 			"accept: application/json",
+	// 			"Content-Type: multipart/form-data",
+	// 		],
+	// 	]);
+		
+	// 	$response = curl_exec($curl);
+	// 	curl_close($curl);
+	
+	// 	$response .= curl_error($curl);
+
+	// 	$suggestionResult = $response; // TODO return some structure with the result or error not being able to open the file
+	// }
+}
+else {
+	$showInitial = true;
+}
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/.private/assembler.php';
+
+$a = new Assembler('Suggestions for Interviews');
+
+$a->body_main = function() use (
+	$showInitial,
+	$showFileUploadForm,
+	$mode,
+	$showSuggestionsResult,
+	$suggestionResult,
+) {
 	if($showInitial) {
 		?>
-		<main>
-			<p>You have the ability to upload your resume and we will suggest what companies are more likely suited for your needs, so you can apply more precisely for interviews.</p>
-			<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-				<fieldset>
-					<legend>Conitnue as</legend>
-					<label for="guest">
-						<input type="radio" name="mode" id="guest" value="<?php echo MODE_GUEST ?>" required>Guest
-					</label>
-					<label for="login">
-						<input type="radio" name="mode" id="login" value="<?php echo MODE_LOGIN ?>" required>UoP Student via Login
-					</label>
-					<hr>
-					<input type="submit" name="submit" value="Proceed">
-				</fieldset>
-			</form>
-			<p>If you wish to log in, we'll store your uploaded resume so that when you're about to start an interview, your interviewer will receive your resume automatically, else your resume will be used only for the suggestions.</p>
-		</main>
+		<p>You have the ability to upload your resume and we will suggest what companies are more likely suited for your needs, so you can apply more precisely for interviews.</p>
+		<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+			<fieldset>
+				<legend>Conitnue as</legend>
+				<label for="guest">
+					<input type="radio" name="mode" id="guest" value="<?php echo MODE_GUEST ?>" required>Guest
+				</label>
+				<label for="login">
+					<input type="radio" name="mode" id="login" value="<?php echo MODE_LOGIN ?>" required>UoP Student via Login
+				</label>
+				<hr>
+				<input type="submit" name="submit" value="Proceed">
+			</fieldset>
+		</form>
+		<p>If you wish to log in, we'll store your uploaded resume so that when you're about to start an interview, your interviewer will receive your resume automatically, else your resume will be used only for the suggestions.</p>
 		<?php
 	}
 	else if($showFileUploadForm) {
 		?>
-		<main>
-			<p>Upload your resume (as .pdf or .docx) and we will suggest what companies are more likely suited for your needs.</p>
-			<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
-				<fieldset>
-					<legend>Your Resume</legend>
-					<label for="resume">
-						<input type="file" name="resume" accept=".pdf, .docx" required>
-					</label>
-					<hr>
-					<input type="submit" name="file_upload" value="Upload">
-				</fieldset>
-			</form>
-			<?php
-				if($mode == MODE_GUEST) {
-					?><p>Your resume will not be stored for later use.</p><?php
-				}
-				else if($mode == MODE_LOGIN) {
-					?><p>Your resume will be stored for later use as mentioned, under the email address you logged in with.</p><?php
-					// TODO add the email received from the SSO
-				}
-			?>
-		</main>
+		<p>Upload your resume (as .pdf or .docx) and we will suggest what companies are more likely suited for your needs.</p>
+		<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
+			<fieldset>
+				<legend>Your Resume</legend>
+				<label for="resume">
+					<input type="file" name="resume" accept=".pdf, .docx" required>
+				</label>
+				<hr>
+				<input type="submit" name="file_upload" value="Upload">
+			</fieldset>
+		</form>
+		<?php
+			if($mode == MODE_GUEST) {
+				?><p>Your resume will not be stored for later use.</p><?php
+			}
+			else if($mode == MODE_LOGIN) {
+				?><p>Your resume will be stored for later use as mentioned, under the email address you logged in with.</p><?php
+				// TODO add the email received from the SSO
+			}
+		?>
 		<?php
 	}
 	else if($showSuggestionsResult) {
 		?>
-		<main>
-			<p>
-			<?php
-				echo $suggestionResult;
-			?>
-			</p>
-		</main>
+		<p>
+		<?php
+			echo $suggestionResult;
+		?>
+		</p>
 		<?php
 	}
-	?>
+};
 
-	<hr>
-	
-	<div style="flex-grow: 1"></div>
-	<?php include $_SERVER['DOCUMENT_ROOT'] . '/.private/components/footer.php'; ?>
-</body>
-</html>
+$a->assemble();
