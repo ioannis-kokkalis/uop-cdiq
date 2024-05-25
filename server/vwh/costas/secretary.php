274fd78c4e02c1bdfb286bee909d43c901e33036
Ioannis Kokkalis <ioanniskokkalis2001@gmail.com>
2024-05-25 18:18:15 +0300

    test: suggestions api

--- server/vwh/costas/secretary.php
+++ server/vwh/costas/secretary.php
@@ -78,30 +78,29 @@ if (isset($_GET['interviewer_id'])) { // interviewer job positions management
 			something_went_wrong();
 		}
 
-		// $curl = curl_init();
+		$curl = curl_init();
 
-		// curl_setopt_array($curl, [
-		// 	CURLOPT_URL => "https://api/kati",
-		// 	CURLOPT_RETURNTRANSFER => true,
-		// 	CURLOPT_ENCODING => "",
-		// 	CURLOPT_MAXREDIRS => 10,
-		// 	CURLOPT_TIMEOUT => 30,
-		// 	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		// 	CURLOPT_CUSTOMREQUEST => "POST",
-		// 	CURLOPT_POSTFIELDS => json_encode($jobs_id_description),
-		// 	CURLOPT_HTTPHEADER => [
-		// 		"Content-Type: application/json"
-		// 	],
-		// ]);
+		curl_setopt_array($curl, [
+			CURLOPT_PORT => "8000",
+			CURLOPT_URL => "http://api/classify_job_descriptions",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => json_encode($jobs_id_description),
+			CURLOPT_HTTPHEADER => [
+				"Content-Type: application/json"
+			],
+		]);
+		
+		$response = curl_exec($curl);
+		if (curl_error($curl)) {
+			something_went_wrong();
+		}
 
-		// $response = curl_exec($curl);
-		// if (curl_error($curl)) {
-		// 	something_went_wrong();
-		// }
-
-		// curl_close($curl);
-
-		$response = '[{"id":"15","tag":"information & technology"},{"id":"2","tag":"information & technology"}, {"id":"16","tag":"ggg"}]';
+		curl_close($curl);
 
 		$response = json_decode($response, true);
 
