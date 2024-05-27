55d0f5a43fbe59ba843b69488e7014a8ce5d08fa
Ioannis Kokkalis <ioanniskokkalis2001@gmail.com>
2024-05-27 17:22:06 +0300

    update: partial index update

--- server/vwh/index.php
+++ server/vwh/index.php
@@ -4,12 +4,26 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/.private/assembler.php';
 
 $a = new Assembler('Career Day 2024<br>Interviews');
 
+$a->body_main_id = 'index-main';
+
 $a->body_main = function() { ?>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget neque id nisi placerat interdum sed pellentesque velit. Nunc a accumsan massa. Maecenas semper tellus justo, at malesuada arcu maximus ac. Vivamus luctus vehicula orci, sed scelerisque ipsum viverra a. Cras facilisis, eros ut molestie hendrerit, metus elit sollicitudin augue, nec scelerisque est nisl quis est. Cras eleifend pellentesque scelerisque. Vivamus nec leo sollicitudin, laoreet velit at, dignissim arcu. Nunc malesuada neque non auctor pellentesque. Nunc iaculis, augue vitae pharetra ullamcorper, dolor leo congue nisi, a imperdiet tellus nibh nec dui. Phasellus et viverra massa.</p>
-	<p>Sed fermentum tempus gravida. Nunc lectus ante, dapibus sit amet neque nec, sollicitudin posuere metus. Etiam ac blandit metus. Sed et posuere ligula. Duis consectetur mi nec eros vehicula luctus. Mauris mi nunc, mattis et euismod sit amet, cursus quis nulla. Curabitur feugiat eget ante ac auctor. Pellentesque commodo a diam id auctor. Duis orci velit, eleifend sed viverra eget, congue et ante. Duis quis augue lectus. Mauris viverra risus et sem commodo, sed placerat felis maximus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
-	<p>Integer varius ante eget erat auctor, non eleifend est ultricies. Duis nisl turpis, tempus in aliquam ac, volutpat quis leo. Nam ligula tellus, lacinia sit amet elementum sit amet, finibus id est. Sed a nulla euismod lorem dignissim interdum. Etiam turpis nulla, tempor at pulvinar nec, accumsan in metus. Proin rutrum, enim sed porttitor malesuada, arcu tellus interdum purus, sed pellentesque mauris eros a orci. Morbi ligula nunc, lacinia id ullamcorper et, molestie sit amet nisl. Donec mattis nisl nulla, vitae egestas nisl viverra non. Ut eu tincidunt ex. In efficitur pretium varius. Aenean tincidunt volutpat euismod. Pellentesque venenatis mi urna, at tincidunt lorem ullamcorper in. Nunc bibendum efficitur magna semper scelerisque.</p>
-	<p>Cras quis lobortis mi, id gravida quam. Sed lobortis elit in sagittis pellentesque. Nam molestie nunc a diam egestas malesuada. Nullam ac feugiat nibh. Vestibulum ultricies tempus est fermentum pharetra. Donec et lacus non diam facilisis vulputate id eget velit. Donec placerat nisl et nulla tempor feugiat. Integer erat tortor, suscipit et porttitor eget, accumsan eu elit. Fusce pretium laoreet dolor vel fermentum. Morbi sem urna, fermentum ut leo sit amet, bibendum scelerisque justo.</p>
-	<p>Quisque vel sapien sit amet turpis tincidunt mattis posuere sed quam. Ut quis eros suscipit, pulvinar justo a, pharetra nisl. Integer porttitor dapibus tristique. Mauris fermentum, orci in tristique placerat, mi mauris semper urna, id luctus arcu mauris a felis. Quisque vel sodales risus. Vivamus vitae tortor dapibus, tristique est non, lobortis libero. Donec suscipit est purus, eget pulvinar arcu tempus sed. Maecenas finibus tempor nibh et consequat. Etiam mollis, enim vitae commodo volutpat, mauris metus ullamcorper mi, sed auctor ex quam ut quam. Cras tellus libero, finibus at maximus eu, vulputate at est. Praesent at sem dapibus, interdum turpis ut, ornare dolor. Suspendisse condimentum felis ut vestibulum placerat. Ut malesuada, nibh a pretium ornare, felis tellus consectetur est, consequat molestie dui urna et quam. Praesent ut lorem congue velit vulputate eleifend.</p>
+	<h1>What is this place?</h1>
+
+	<p>Here you can stay up to date with all interviews happening during the event. This includes currently happening interviews, interviews in each company's queue, and their related states at any moment.</p>
+	<p>Furthermore, we can recommend to you positions and companies that fit you best based on your resume.</p>
+	<p>Navigate from the menu on top to explore its feature!</p>
+
+	<h1>Interview where?</h1>
+
+	<p>TBD</p>
+
+	<h1>Share</h1>
+	
+	<p>Use the QR code...</p>
+	<script src="/script/utilities.js"></script>
+	<img id="current_url_qr" alt="...it did not generated properly...">
+	<script>qr_generate(window.location.href, document.getElementById('current_url_qr'));</script>
+	<p>...or <a href="#3werf" id="3werf">copy the link</a> and send it!</p>
 <?php };
 
 $a->assemble();
